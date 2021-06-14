2c67d28c658940406011142e0790815b4a28a20a
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-06-13 20:50:02 -0500

    Completo informacion. Pagina para visualizar certificados. Animaciones.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ static/js/certificates.js
@@ -0,0 +1,23 @@
+async function loadCertificates() {
+    const response = await fetch('../static/json/certificates.json');
+    const json = await response.json();
+
+    const table = document.getElementById('certificates');
+
+    table.innerHTML = json.certificates.map((certificate, index) => `
+        <tr>
+            <td>${index + 1}</td>
+            <td>${certificate.title}</td>
+            <td>${certificate.description}</td>
+            <td>
+                <a href="${certificate.path}">Ver más</a>
+            </td>
+        </tr>
+    `
+    ).join('');
+
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    loadCertificates();
+});
---
+++ static/js/index.js
@@ -0,0 +1,22 @@
+let links = document.querySelectorAll('a');
+
+for (let a of links) {
+    a.addEventListener('click', (event) => {
+
+        event.preventDefault(); // Anula el comportamiendo por defecto de un elemento.
+
+        let content = document.querySelector('.card'); // Busca un elemento por la clase content.
+
+        content.classList.remove('animate__fadeInLeft'); // Quita la animacion de entrada.
+
+        content.classList.add('animate__fadeOutLeft'); // Agrega una animacion de salida.
+
+        // Navega a la pagina del href luego de 500 milisegundos.
+        setTimeout(
+            function () {
+                location.href = a.href;
+            },
+            500
+        );
+    });
+}
