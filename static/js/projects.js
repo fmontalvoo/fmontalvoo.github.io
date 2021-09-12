178d28d04197d033ee2c97d63abf0e6f31e0f0d0
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-09-12 16:20:22 -0500

    Se agrega pagina para proyectos.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ static/js/projects.js
@@ -0,0 +1,23 @@
+async function loadCertificates() {
+    const response = await fetch('../static/json/projects.json');
+    const json = await response.json();
+
+    const table = document.getElementById('projects');
+
+    table.innerHTML = json.projects.map((project, index) => `
+        <tr>
+            <td>${index + 1}</td>
+            <td>${project.title}</td>
+            <td>${project.description}</td>
+            <td>
+                <a href="${project.path}" target="_blank">Ver más</a>
+            </td>
+        </tr>
+    `
+    ).join('');
+
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    loadCertificates();
+});
